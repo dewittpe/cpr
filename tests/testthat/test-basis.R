42dde213bab9891086f64f77c8a832796967b7b2
Peter DeWitt (optiplex) <peter.dewitt@ucdenver.edu>
2015-10-21 13:43:33 -0600

    corrected testing script

--- tests/testthat/test-basis.R
+++ tests/testthat/test-basis.R
@@ -1,6 +1,6 @@
 test_that("Equivalent Basis Matrix", 
           { 
-            expect_equivalent(generate_bsplines(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), k = 4)[[1]],
+            expect_equivalent(cpr::bs(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), order = 4)[[1]],
                               matrix(splines::bs(0:10, knots = c(2, 2.6, 7.8), Boundary.knots = c(0, 10), intercept = TRUE)))
           }
 )
