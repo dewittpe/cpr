6d8c30ac712ad10eb1195461a0812f0e65d030bc
Peter DeWitt (optiplex) <peter.dewitt@ucdenver.edu>
2015-10-21 12:49:02 -0600

    added basic package documentation

---
+++ R/cpr.R
@@ -0,0 +1,8 @@
+#' cpr: Control Polygon Reduction
+#'
+#' The cpr package implements the control polygon reduction method presented in
+#' MY TO BE PUBLISHED paper(s).
+#'
+#' @docType package
+#' @name cpr
+NULL
