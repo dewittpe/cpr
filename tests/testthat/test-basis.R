75e3ecdcada3188eb4e42c57ab416f2ea2a26c9f
Peter DeWitt (inspiron-5323) <dewittpe@gmail.com>
2015-10-23 13:30:07 -0600

    test passes

--- tests/testthat/test-basis.R
+++ tests/testthat/test-basis.R
@@ -1,6 +1,9 @@
+require(splines)
+
 test_that("Equivalent Basis Matrix", 
           { 
-            expect_equivalent(cpr::bsplines(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), order = 4),
-                              matrix(splines::bs(0:10, knots = c(2, 2.6, 7.8), Boundary.knots = c(0, 10), intercept = TRUE)))
+            expect_equivalent(unclass(cpr::bsplines(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), order = 4)),
+                              unclass(splines::bs(0:10, knots = c(2, 2.6, 7.8), Boundary.knots = c(0, 10), intercept = TRUE)))
           }
 )
+
