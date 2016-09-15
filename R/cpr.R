1d567efb44075f47720a45f9bec389f32730388b
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2016-09-14 18:03:27 -0600

    re #6: summary.cpr_cpr reports the following in a data_frame
    * degress of freedom of the spline,
    * the iknot locations
    * the log likelihood for each fit
    * the rmse for each fit

--- R/cpr.R
+++ R/cpr.R
@@ -72,5 +72,8 @@ is.cpr_cpr <- function(x) {
 #' @param object a \code{cpr_cpr} object
 #' @rdname cpr
 summary.cpr_cpr <- function(object, ...) {
-  print(object, ...) 
-}
+  dplyr::data_frame(dfs    = sapply(object, function(x) length(x$theta)),
+                    iknots = sapply(object, function(x) x$iknots),
+                    loglik = sapply(object, function(x) x$loglik),
+                    rmse   = sapply(object, function(x) x$rmse))
+} 
