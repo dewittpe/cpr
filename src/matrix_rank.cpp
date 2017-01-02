3a3efb50ae4ab7d10d17a7421c1df05c6c66b1c2
Peter DeWitt (optiplex) <peter.dewitt@ucdenver.edu>
2017-01-02 09:41:04 -0700

    matrix_rank.cpp added

---
+++ src/matrix_rank.cpp
@@ -0,0 +1,33 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+
+//' Rank of a Matrix
+//'
+//' Determine the rank (number of linearly independent columns) of a matrix.
+//'
+//' Implimentation via the Armadillo C++ linear algrebra library.  The function
+//' returns the rank of the matix \code{x}.  The computation is based on the
+//' singular value decomposition of the matrix; a std::runtime_error excetion
+//' will be thrown if the decomposition fails.  Any singular values less than
+//' the tolerance are treated as zeros.  The tolerance is max(m, n) * max_sv *
+//' datum::eps, where m is the number of rows of x, n is the number of columns
+//' of x, max_sv is the maximal singular value of x, and datum::eps is the
+//' difference between 1 and the least value greater than 1 that is
+//' representable.
+//'
+//' @param x a numeric matrix
+//'
+//' @return
+//' the rank of the matrix as a numeric value.
+//'
+//' @references
+//'
+//' Conrad Sanderson and Ryan Curtin.  Armadillo: a template-based C++ library for linear algebra.  Journal of Open Source Software, Vol. 1, pp. 26, 2016.
+//'
+//' @export
+// [[Rcpp::export]]
+double matrix_rank(NumericMatrix x) {
+  arma::mat X(x.begin(), x.nrow(), x.ncol(), false);
+  return arma::rank(X);
+}
