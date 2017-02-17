80f7efaaa2b8fdfef4e58f3c83a012ff5098e1d7
Peter DeWitt (latitude) <peter.dewitt@ucdenver.edu>
2017-02-16 21:57:01 -0700

    moved examples for bsplines into the examples/ dir

--- R/bsplines.R
+++ R/bsplines.R
@@ -10,6 +10,9 @@
 #' \code{cpr::bsplines} is in C++ and tends to be faster than
 #' \code{splines::bs}.
 #'
+#' See the \code{vignette("cpr-pkg", package = "cpr")} for a detailed
+#' comparison between the \code{bsplines} and \code{\link[splines]{bs}} calls.
+#'
 #' @references
 #' C. de Boor, "A practical guide to splines. Revised Edition," Springer, 2001.
 #'
@@ -27,30 +30,12 @@
 #' @seealso \code{\link{plot.cpr_bs}} for plotting the basis,
 #' \code{\link{bsplineD}} for building the basis matrices for the first and
 #' second derivative of a B-spline.
+#' 
+#' See \code{\link{update_bsplines}} for info on a tool for updating a
+#' \code{cpr_bs} object.  This is a similar method to the
+#' \code{\link[stats]{update}} function from the \code{stats} package.
 #'
-#' @examples
-#' # build a vector of values to transform
-#' xvec <- seq(-3, 5, length = 100)
-#'
-#' # cubic b-spline
-#' bmat <- bsplines(xvec, iknots = c(-2, 0, 0.2))
-#' bmat
-#'
-#' # view the splines
-#' plot(bmat)
-#'
-#' # If you want a second x-axis to show the x-values try the following:
-#' second_x_axis <- round(stats::quantile(xvec, probs = seq(0, 1, by = .2)), 2)
-#'
-#' plot(bmat) +
-#' ggplot2::annotate(geom = "text", x = second_x_axis, y = -0.02, label = second_x_axis) +
-#' ggplot2::annotate(geom = "linerange", x = second_x_axis, ymin = -0.05, ymax = -0.04) +
-#' ggplot2::coord_cartesian(ylim = c(0, 1))
-#'
-#' # quadratic splines
-#' bmat <- bsplines(xvec, iknots = c(-2, 0, 0.2), order = 3L)
-#' bmat
-#' plot(bmat) + ggplot2::ggtitle("Quadratic B-splines")
+#' @example examples/bsplines.R
 #'
 #' @export
 bsplines <- function(x, iknots = NULL, df = NULL, bknots = range(x), order = 4L) {
