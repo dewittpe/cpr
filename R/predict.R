683b525748802c79d697f322ed634c1b20a06d4a
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-01-21 16:45:40 -0700

    remove dplyr calls

--- R/predict.R
+++ R/predict.R
@@ -14,9 +14,8 @@ predict.cpr_cp <- function(object, newdata, ...) {
   f_for_use <- data_for_use <- NULL
   generate_cp_formula_data(updatebsplines(stats::formula(object), object$iknots, object$bknots, object$order), newdata)
   XMAT <- stats::model.matrix(lme4::nobars(f_for_use)[-2], data_for_use)
-  dplyr::data_frame(pred = as.numeric(XMAT %*% object$coefficients),
-                    se   = apply(XMAT, 1, function(x, sg) {sqrt(matrix(x, nrow = 1) %*% sg %*% matrix(x, ncol = 1))}, sg = object$vcov)
-                    ) 
+  data.frame(pred = as.numeric(XMAT %*% object$coefficients),
+             se   = apply(XMAT, 1, function(x, sg) {sqrt(matrix(x, nrow = 1) %*% sg %*% matrix(x, ncol = 1))}, sg = object$vcov))
 }
 
 #' @export
@@ -24,17 +23,16 @@ predict.cpr_cn <- function(object, newdata, ...) {
 
   iks <- lapply(object$bspline_list, attr, which = "iknots")
   bks <- lapply(object$bspline_list, attr, which = "bknots")
-  ods <- lapply(object$bspline_list, attr, which = "order") 
+  ods <- lapply(object$bspline_list, attr, which = "order")
 
-  f_for_use <- data_for_use <- NULL 
-  generate_cp_formula_data(updatebsplines(stats::formula(object), iks, bks, ods), newdata) 
+  f_for_use <- data_for_use <- NULL
+  generate_cp_formula_data(updatebsplines(stats::formula(object), iks, bks, ods), newdata)
   XMAT <- stats::model.matrix(lme4::nobars(f_for_use)[-2], data_for_use)
-  dplyr::data_frame(pred = as.numeric(XMAT %*% object$coefficients),
-                    se   = apply(XMAT, 1, function(x, sg) {sqrt(matrix(x, nrow = 1) %*% sg %*% matrix(x, ncol = 1))}, sg = object$vcov)
-                    ) 
+  data.frame(pred = as.numeric(XMAT %*% object$coefficients),
+             se   = apply(XMAT, 1, function(x, sg) {sqrt(matrix(x, nrow = 1) %*% sg %*% matrix(x, ncol = 1))}, sg = object$vcov))
 }
 
-updatebsplines <- function(form, nik, nbk, nord) { 
+updatebsplines <- function(form, nik, nbk, nord) {
   rr <- function(x, nik, nbk, nord) {
     if(is.call(x) && grepl("bsplines|btensor", deparse(x[[1]]))) {
       x$df <- NULL
@@ -49,7 +47,7 @@ updatebsplines <- function(form, nik, nbk, nord) {
     }
   }
 
-  z <- lapply(as.list(form), rr, nik, nbk, nord)   
+  z <- lapply(as.list(form), rr, nik, nbk, nord)
   z <- eval(as.call(z))
   environment(z) <- environment(form)
   z
