e2a4ea7cb3bb3102b553a309b012784b22baa2d8
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-01-21 08:19:33 -0700

    replace dplyr::filter_ with base subset

--- R/cnr_diagnostics.R
+++ R/cnr_diagnostics.R
@@ -2,40 +2,40 @@
 #'
 #' A collection of function for the inspection and evaluation of the control
 #' polygon reduction.
-#' 
+#'
 #' @author Peter DeWitt \email{dewittpe@gmail.com}
 #'
 #' @method plot cpr_cnr
 #' @rdname cnr_diagnostics
-#' @export 
+#' @export
 #' @param x a \code{cpr_cnr} object
 #' @param type type of diagnostic plot.
-#' \code{"loglik"} for the loglikihood by degrees of freedom, 
+#' \code{"loglik"} for the loglikihood by degrees of freedom,
 #' \code{"rmse"} for root mean squared residuals by model index
 #' @param from the first index of \code{x} to plot
 #' @param to the last index of \code{x} to plot
 #' @param ... ignored
-plot.cpr_cnr <- function(x, type = "rmse", from = 1, to, ...) { 
+plot.cpr_cnr <- function(x, type = "rmse", from = 1, to, ...) {
 
-  if (from < 1) { 
+  if (from < 1) {
     from <- 1
-  } 
+  }
 
-  if (any(type %in% c("rmse", "loglik"))) { 
-    if (missing(to)) { 
+  if (any(type %in% c("rmse", "loglik"))) {
+    if (missing(to)) {
       to <- length(x)
-    } else if (to > length(x)) { 
+    } else if (to > length(x)) {
       to <- length(x)
     }
 
-    ggplot2::ggplot(dplyr::filter_(summary(x), .dots = ~ dplyr::between(index, from, to))) + 
-    ggplot2::theme_bw() + 
-    ggplot2::aes_string(x = "index", y = type) + 
-    ggplot2::geom_point() + 
+    ggplot2::ggplot(subset(x, (x$index >= from) & (x$index <= to))) +
+    ggplot2::theme_bw() +
+    ggplot2::aes_string(x = "index", y = type) +
+    ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::xlab("Index")
 
-  } else { 
+  } else {
     stop("type needs to be either 'loglik' or 'rmse'.")
   }
 }
--- R/cpr_diagnostics.R
+++ R/cpr_diagnostics.R
@@ -2,7 +2,7 @@
 #'
 #' A collection of function for the inspection and evaluation of the control
 #' polygon reduction.
-#' 
+#'
 #'
 #' @author Peter DeWitt \email{dewittpe@gmail.com}
 #'
@@ -11,51 +11,51 @@
 
 #' @method plot cpr_cpr
 #' @rdname cpr_diagnostics
-#' @export 
+#' @export
 #' @param x a \code{cpr_cpr} object
 #' @param type type of diagnostic plot.  \code{"cps"} for control polygons,
-#' \code{"loglik"} for the loglikihood by degrees of freedom, 
+#' \code{"loglik"} for the loglikihood by degrees of freedom,
 #' \code{"rmse"} for root mean squared residuals by degrees of freedom
 #' @param from the first index of \code{x} to plot
 #' @param to the last index of \code{x} to plot
 #' @param ... args passed to \code{cpr::plot.cpr_cp}
 #' @seealso \code{\link{plot.cpr_cp}}
-plot.cpr_cpr <- function(x, type = "cps", from = 1, to, ...) { 
+plot.cpr_cpr <- function(x, type = "cps", from = 1, to, ...) {
 
-  if (from < 1) { 
+  if (from < 1) {
     from <- 1
-  } 
+  }
 
-  if (type == "cps") { 
+  if (type == "cps") {
 
-    if (missing(to)) { 
+    if (missing(to)) {
       to <- 6
-    } else if (to > length(x)) { 
+    } else if (to > length(x)) {
       to <- length(x)
     }
 
     nm <- deparse(substitute(x))
 
     eval(parse(text = paste("plot(",
-                            paste(paste0(nm, "[[", seq(from = from, to = to, by = 1), "]]"), 
+                            paste(paste0(nm, "[[", seq(from = from, to = to, by = 1), "]]"),
                                   collapse = ", "),
                             ", ...)")))
 
-  } else if (any(type %in% c("rmse", "loglik"))) { 
-    if (missing(to)) { 
+  } else if (any(type %in% c("rmse", "loglik"))) {
+    if (missing(to)) {
       to <- length(x)
-    } else if (to > length(x)) { 
+    } else if (to > length(x)) {
       to <- length(x)
     }
 
-    ggplot2::ggplot(dplyr::filter_(summary(x), .dots = ~ dplyr::between(index, from, to))) + 
-    ggplot2::theme_bw() + 
-    ggplot2::aes_string(x = "index", y = type) + 
-    ggplot2::geom_point() + 
+    ggplot2::ggplot(subset(x, (x$index >= from) & (x$index <= to))) +
+    ggplot2::theme_bw() +
+    ggplot2::aes_string(x = "index", y = type) +
+    ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::xlab("Index")
 
-  } else { 
+  } else {
     stop("type needs to be either 'cps', 'loglik', or 'rmse'.")
   }
 }
