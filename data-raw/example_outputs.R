90ecfec7eb5372669f348dd0141aac5a8b9eedb7
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-02-01 12:31:12 -0700

    reinstate test for cnr

--- data-raw/example_outputs.R
+++
@@ -1,9 +0,0 @@
-# The following objects are used to reduce the time required for tests and
-# building vignettes.
-
-
-library(cpr)
-initial_cp <- cp(log10(pdg) ~ bsplines(day, df = 54), data = spdg)
-cpr_run <- cpr(initial_cp)
-saveRDS(cpr_run, "./inst/example_objects/expected_cpr_run.rds")
-
