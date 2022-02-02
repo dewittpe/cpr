d56b156d7f3afd9564d84ab1a4037f51dd1056b8
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-02-02 11:54:14 -0700

    clean up testing for bspline basis

--- tests/testthat/test-basis.R
+++ tests/testthat/test-basis.R
@@ -1,42 +1,42 @@
 require(splines)
 
-test_that("Equivalent Basis Matrix", 
-          { 
-            expect_equivalent(unclass(cpr::bsplines(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), order = 4)),
+test_that("Equivalent Basis Matrix",
+          {
+            expect_equivalent(unclass(bsplines(0:10, iknots = c(2, 2.6, 7.8), bknots = c(0, 10), order = 4)),
                               unclass(splines::bs(0:10, knots = c(2, 2.6, 7.8), Boundary.knots = c(0, 10), intercept = TRUE)))
           }
 )
 
-test_that("Knots will be sorted", 
+test_that("Knots will be sorted",
           {
             xvec <- seq(-1, 1, length = 25)
             iknots <- c(0.34, -0.23)
 
-            expect_warning(cpr::bsplines(xvec, iknots = iknots), "Sorting knots")
+            expect_warning(bsplines(xvec, iknots = iknots), "Sorting knots")
 
-            expect_equivalent(unclass(suppressWarnings(cpr::bsplines(xvec, iknots = c(.34, -.23)))),
-                              unclass(splines::bs(xvec, knots = iknots, intercept = TRUE))) 
+            expect_equivalent(unclass(suppressWarnings(bsplines(xvec, iknots = c(.34, -.23)))),
+                              unclass(splines::bs(xvec, knots = iknots, intercept = TRUE)))
           })
 
-test_that("x is not a list", 
+test_that("x is not a list",
           {
-            expect_error(cpr::bsplines(list(1:10)), "btensor")
+            expect_error(bsplines(list(1:10)), "btensor")
           })
 
 test_that("combinations of iknots and df are handled well",
           {
             xvec <- seq(-1, 1, length = 25)
-            expect_warning(cpr::bsplines(xvec, df = 2), "df being set to order")
-            expect_equivalent(suppressWarnings(cpr::bsplines(xvec, df = 2)), cpr::bsplines(xvec, df = 4))
-            expect_equivalent(suppressWarnings(cpr::bsplines(xvec, df = 4, order = 5)), cpr::bsplines(xvec, df = 5, order = 5))
-            expect_equivalent(suppressWarnings(cpr::bsplines(xvec, df = 4, order = 5)), cpr::bsplines(xvec, order = 5))
+            expect_warning(bsplines(xvec, df = 2), "df being set to order")
+            expect_equivalent(suppressWarnings(bsplines(xvec, df = 2)), bsplines(xvec, df = 4))
+            expect_equivalent(suppressWarnings(bsplines(xvec, df = 4, order = 5)), bsplines(xvec, df = 5, order = 5))
+            expect_equivalent(suppressWarnings(bsplines(xvec, df = 4, order = 5)), bsplines(xvec, order = 5))
 
-            expect_warning(cpr::bsplines(xvec, iknots = 0.2, df = 6), "using iknots")
-            expect_equivalent(suppressWarnings(cpr::bsplines(xvec, iknots = 0.2, df = 6)),
-                              cpr::bsplines(xvec, iknots = 0.2)) 
+            expect_warning(bsplines(xvec, iknots = 0.2, df = 6), "using iknots")
+            expect_equivalent(suppressWarnings(bsplines(xvec, iknots = 0.2, df = 6)),
+                              bsplines(xvec, iknots = 0.2))
 
-            expect_equivalent(cpr::bsplines(xvec, df = 7), 
-                              cpr::bsplines(xvec, iknots = trimmed_quantile(xvec, probs = 1:3/4)))
+            expect_equivalent(bsplines(xvec, df = 7),
+                              bsplines(xvec, iknots = trimmed_quantile(xvec, probs = 1:3/4)))
           })
 
 test_that("derivatives are as expected.",
