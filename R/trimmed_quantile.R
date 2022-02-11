b752fd279e8804154bb47ff7a9aa4d392360073e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-02-11 10:22:14 -0700

    extend testing for trimmed_quantile

--- R/trimmed_quantile.R
+++ R/trimmed_quantile.R
@@ -1,13 +1,13 @@
 #' Trimmed Quantiles
 #'
 #' For data X = x1, x2, ..., xn, with order statistics x(1), x(2), ..., x(r) return
-#' the quantiles for a trimmed data set, e.g., X \ {x(1), x(r)} (trim = 1), or 
+#' the quantiles for a trimmed data set, e.g., X \ {x(1), x(r)} (trim = 1), or
 #' X \ {x(1), x(2), x(r-1), x(r)} (trim = 2).
 #'
 #' @param x a numeric vector
 #' @param trim defaults to 1, omitting the min and the max
 #' @param use_unique logical, if true (defaults), base the quantiles on unique
-#' values, if false, base the quantiles on all data, after trimming.   
+#' values, if false, base the quantiles on all data, after trimming.
 #' @param ... other arguments to pass to stats::quantile
 #'
 #'
@@ -21,7 +21,7 @@
 #' trimmed_quantile(1:100, trim = 3, prob = 1:23 / 24, name = FALSE)
 #'
 #' @export
-trimmed_quantile <- function(x, trim = 1L, use_unique = TRUE, ...) { 
+trimmed_quantile <- function(x, trim = 1L, use_unique = TRUE, ...) {
   if (trim < 1) {
     trim <- 1L
     warning("Overruling trim less than 1 with trim = 1L")
@@ -33,14 +33,14 @@ trimmed_quantile <- function(x, trim = 1L, use_unique = TRUE, ...) {
 
   this_x <- x
 
-  if (use_unique) { 
+  if (use_unique) {
     this_x <- unique(this_x)
   }
 
-  for(i in seq(from = 1L, to = trim, by = 1)) { 
+  for(i in seq(from = 1L, to = trim, by = 1)) {
     this_x <- this_x[!(this_x %in% range(this_x))]
   }
 
-  stats::quantile(this_x, ...) 
+  stats::quantile(this_x, ...)
 }
 
--- tests/testthat/test-trimmed_quantile.R
+++ tests/testthat/test-trimmed_quantile.R
@@ -5,4 +5,17 @@ test_that("trimmed quantile warnings work",
             expect_equivalent(suppressWarnings(trimmed_quantile(1:100, trim = 3.9, prob = 1:23 / 24, name = FALSE)),
                               quantile(4:97, prob = 1:23 / 24)
                               )
-          }) 
+          })
+
+test_that("trimmed quantile results are as expected",
+          {
+            x <- sample(runif(100, 0, 6), size = 500, replace = TRUE)
+            ux <- unique(x)
+
+            xt <- x[!(x %in% range(x))]
+            uxt <- ux[!(ux %in% range(ux))]#-c(which.min(ux), which.max(ux))]
+
+            expect_equivalent(trimmed_quantile(x), quantile(uxt))
+            expect_equivalent(trimmed_quantile(x, use_unique = FALSE), quantile(xt))
+
+          })
