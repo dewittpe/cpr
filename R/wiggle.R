237fbb9a47ec061bb228d2d15c057f27037fdd40
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-12-13 17:33:37 -0700

    add tests and examples for wiggle

--- R/wiggle.R
+++ R/wiggle.R
@@ -9,11 +9,31 @@
 #' @param lower the lower limit of the integral
 #' @param upper the upper limit of the integral
 #' @param stop.on.error default to \code{FALSE}, see \code{\link[stats]{integrate}}.
-#' @param ... arguments passed to \code{stats::integrate}
+#' @param ... arguments passed to \code{\link[stats]{integrate}}
+#'
+#' @return Same as \code{\link[stats]{integrate}}.
 #'
 #' @seealso \code{\link{cp}} \code{\link[stats]{integrate}}
 #'
-
+#' @examples
+#' xvec <- seq(0, 6, length = 500)
+#'
+#' # Define the basis matrix
+#' bmat1 <- bsplines(x = xvec, iknots = c(1, 1.5, 2.3, 4, 4.5))
+#' bmat2 <- bsplines(x = xvec)
+#'
+#' # Define the control vertices ordinates
+#' theta1 <- c(1, 0, 3.5, 4.2, 3.7, -0.5, -0.7, 2, 1.5)
+#' theta2 <- c(1, 3.4, -2, 1.7)
+#'
+#' # build the two control polygons
+#' cp1 <- cp(bmat1, theta1)
+#' cp2 <- cp(bmat2, theta2)
+#' plot(cp1, cp2, show_cp = FALSE, show_spline = TRUE)
+#'
+#' wiggle(cp1)
+#' wiggle(cp2)
+#'
 #' @export
 wiggle <- function(object, lower, upper, stop.on.error = FALSE, ...) {
   UseMethod("wiggle")
---
+++ tests/test-wiggle.R
@@ -0,0 +1,42 @@
+# verify wiggle returns the integeral of the squared second derivative
+
+
+library(cpr)
+
+f <- function(x) {
+  #(x + 2) * (x - 1) * (x - 3)
+  x^3 - 2 * x^2 - 5 * x + 6
+}
+
+fprime <- function(x) { # first derivatives of f(x)
+  3 * x^2 - 4 * x - 5
+}
+
+fdoubleprime <- function(x) { # second derivatives of f(x)
+  6 * x - 4
+}
+
+#'
+#' We'll look at the function over
+bknots = c(-3, 5)
+
+x     <- seq(-3 + 1/100, 5 - 1/100, length.out = 100)
+bmat  <- bsplines(x, bknots = bknots)
+theta <- matrix(coef(lm(f(x) ~ bmat + 0)), ncol = 1)
+
+bmatD1 <- bsplineD(x, bknots = bknots, derivative = 1L)
+bmatD2 <- bsplineD(x, bknots = bknots, derivative = 2L)
+
+fdoubleprime_squared <- function(x) {
+  fdoubleprime(x)^2
+}
+
+stopifnot(isTRUE(all.equal(
+  integrate(fdoubleprime_squared, lower = -3, upper = 5)$value
+  ,
+  wiggle(cp(bmat, theta), lower = -3, upper = 5)$value
+)))
+
+################################################################################
+#                                 End of File                                  #
+################################################################################
