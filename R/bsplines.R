2b6eb82928450a153bbad65d941a12e49a186d70
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-12-20 15:24:32 -0700

    move iknots_or_df from R/bsplines.R to R/utilities.R

--- R/bsplines.R
+++ R/bsplines.R
@@ -323,21 +323,3 @@ bsplineD <- function(x, iknots = NULL, df = NULL, bknots = range(x), order = 4L,
   }
   rtn
 }
-
-iknots_or_df <- function(x, iknots, df, order) {
-  if (is.null(iknots) & is.null(df)) {
-    iknots <- numeric(0)
-  } else if (is.null(iknots) & !is.null(df)) {
-    if (df < order) {
-      warning("df being set to order")
-      iknots <- numeric(0)
-    } else if (df == order) {
-      iknots <- numeric(0)
-    } else {
-      iknots <- trimmed_quantile(x, probs = seq(1, df - order, by = 1) / (df - order + 1))
-    }
-  } else if (!is.null(iknots) & !is.null(df)) {
-    warning("Both iknots and df defined, using iknots")
-  }
-  iknots
-}
---
+++ R/utilities.R
@@ -0,0 +1,35 @@
+# utilities.R - a set of non-exported functions used within other methods in the
+# cpr package
+
+#' Internal Knots or Degrees of Freedom
+#'
+#' Used in the \link{bsplines} call to check order, degrees of freedom (df) and
+#' iknots
+#'
+#' @param x
+#' @param iknots
+#' @param df
+#' @param order
+#'
+#' @return a numeric vector to use as the internal knots defining a B-spline
+#'
+#' @examples
+#'
+#'
+iknots_or_df <- function(x, iknots, df, order) {
+  if (is.null(iknots) & is.null(df)) {
+    iknots <- numeric(0)
+  } else if (is.null(iknots) & !is.null(df)) {
+    if (df < order) {
+      warning("df being set to order")
+      iknots <- numeric(0)
+    } else if (df == order) {
+      iknots <- numeric(0)
+    } else {
+      iknots <- trimmed_quantile(x, probs = seq(1, df - order, by = 1) / (df - order + 1))
+    }
+  } else if (!is.null(iknots) & !is.null(df)) {
+    warning("Both iknots and df defined, using iknots")
+  }
+  iknots
+}
