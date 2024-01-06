75407b6aac0d0bbabb393a9d112a0ebe220fbb44
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-01-06 15:49:17 -0700

    add test for verbose and forking in influence_of_iknots, re #57

--- tests/test-influence_of_iknots.R
+++ tests/test-influence_of_iknots.R
@@ -28,6 +28,34 @@ with(e, {
 })
 
 ################################################################################
+# verbose vs non-versbose
+e <- new.env()
+with(e, {
+  x <- runif(n = 100, 0, 6)
+  bmat <- bsplines(x, iknots = c(1, 1.5, 2.3, 4, 4.5), bknots = c(0, 6))
+  theta <- matrix(c(1, 0, 3.5, 4.2, 3.7, -0.5, -0.7, 2, 1.5), ncol = 1)
+  DF <- data.frame(x = x, truth = as.numeric(bmat %*% theta))
+  DF$y <- as.numeric(bmat %*% theta + rnorm(nrow(bmat), sd = 0.3))
+
+  initial_cp <-
+    cp(y ~ bsplines(x, iknots = c(1, 1.5, 2.3, 3.0, 4, 4.5), bknots = c(0, 6)), data = DF)
+
+  iok0 <- influence_of_iknots(initial_cp, versbose = FALSE, cl = 0)
+  iok1 <- influence_of_iknots(initial_cp, versbose = FALSE, cl = 1)
+  iok2 <- influence_of_iknots(initial_cp, versbose = FALSE, cl = 2)
+  iok0v <- influence_of_iknots(initial_cp, versbose = TRUE, cl = 0)
+  iok1v <- influence_of_iknots(initial_cp, versbose = TRUE, cl = 1)
+  iok2v <- influence_of_iknots(initial_cp, versbose = TRUE, cl = 2)
+
+  stopifnot(isTRUE(all.equal(iok0, iok1)))
+  stopifnot(isTRUE(all.equal(iok0, iok2)))
+  stopifnot(isTRUE(all.equal(iok0, iok0v)))
+  stopifnot(isTRUE(all.equal(iok0, iok1v)))
+  stopifnot(isTRUE(all.equal(iok0, iok2v)))
+
+})
+
+################################################################################
 e <- new.env()
 with(e, {
 
